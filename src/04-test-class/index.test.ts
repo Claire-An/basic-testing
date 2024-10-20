32c070b9f6488d9792b2558617119df6e3873e75
Svetlana <Claire-An@mail.ru>
2024-10-20 19:42:17 +0500

    feat: add test-class tests

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,33 +1,62 @@
-// Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import {
+  BankAccount,
+  getBankAccount,
+  InsufficientFundsError,
+  TransferFailedError,
+} from '.';
 
 describe('BankAccount', () => {
+  let currentAcc: BankAccount;
+  const initBalance = 100;
+
+  beforeEach(() => {
+    currentAcc = getBankAccount(initBalance);
+  });
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(currentAcc.getBalance()).toBe(initBalance);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    expect(() => currentAcc.withdraw(currentAcc.getBalance() + 1)).toThrow(
+      InsufficientFundsError,
+    );
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    const newAcc = getBankAccount(0);
+    expect(() =>
+      currentAcc.transfer(currentAcc.getBalance() + 1, newAcc),
+    ).toThrow(InsufficientFundsError);
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    expect(() => currentAcc.transfer(1, currentAcc)).toThrow(
+      TransferFailedError,
+    );
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const balance = currentAcc.getBalance();
+    const deposit = 50;
+    currentAcc.deposit(deposit);
+    expect(currentAcc.getBalance()).toBe(balance + deposit);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const balance = currentAcc.getBalance();
+    const delta = 1;
+    currentAcc.withdraw(delta);
+    expect(currentAcc.getBalance()).toBe(balance - delta);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const balance = currentAcc.getBalance();
+    const newAcc = getBankAccount(0);
+    const delta = 1;
+    currentAcc.transfer(delta, newAcc);
+    expect(currentAcc.getBalance()).toBe(balance - delta);
+    expect(newAcc.getBalance()).toBe(delta);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
